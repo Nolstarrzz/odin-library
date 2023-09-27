f86f1eb9377e0cd3d099250519d3f7d43d129bd8
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2023-09-26 18:09:53 -0700

    got the js to log the variables,
    need to make those variables make a card in the webpage

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let myLibrary = [];
-let form = document.getElementById("popUpForm");
 
 
 function Book(title, author, pages, read) {
@@ -7,6 +6,7 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+  console.log(read)
   this.info = function() {
     return(title + " by " + author + ", " + pages + ", " + read);
   }
@@ -19,22 +19,49 @@ function addBookToLibrary(title, author, pages, read) {
 
 function loopBooks() {
   for (const book of myLibrary) {
-    console.log(book.info());
+    console.log(book.title + " by " + book.author + ", " + book.pages + ", " + book.read);
   }
 }
-
 function cancelForm()
 {
+  let form = document.getElementById("popUpForm");
   form.style.display = 'none';
 }
 
 function startForm()
 {
+  let form = document.getElementById("popUpForm");
   form.style.display = 'grid';
-  let title = document.getElementById("title");
-  let author = document.getElementById("author");
-  let pages = document.getElementById("pages");
-  let read = document.getElementById("readOption");
-  addBookToLibrary(title, author, pages, read)
+}
+
+function getValues() {
+  let read;
+  let form = document.getElementById("popUpForm");
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read1 = document.getElementById("notRead").checked;
+  let read2 = document.getElementById("read").checked;
+  
+  if (!title || !author || !pages || (!read1 && !read2)) {
+    alert("Please fill out all required fields.");
+    return; // Stop processing the form
+  }
+
+  console.log("Title:", title);
+  console.log("Author:", author);
+  console.log("Pages:", pages);
+  console.log("Read1:", read1);
+  console.log("Read2:", read2);
+
+  if (read1) {
+    read = read1;
+    console.log("Read:", read);
+  } else if (read2) {
+    read = read2;
+    console.log("Read:", read);
+  }
+
+  addBookToLibrary(title, author, pages, read);
   loopBooks();
 }
